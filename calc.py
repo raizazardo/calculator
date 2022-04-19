9523da7d9bbe50ea2908b7d66888ec6f3b7cc3e8
raizazardo <68117249+raizazardo@users.noreply.github.com>
2022-04-19 10:56:30 -0300

    Create calc.py

---
+++ calc.py
@@ -0,0 +1,46 @@
+#Calculator
+from art import logo
+#Add
+def add(n1,n2):
+	return n1 + n2
+#Sutraction
+def subtract(n1, n2):
+	return n1 - n2
+#Multiply
+def multiply(n1, n2):
+	return n1 * n2
+#Divide 
+def divide (n1, n2):
+	return n1/n2
+
+operations = {
+	"+": add,
+	"-": subtract,
+	"*": multiply,
+	"/": divide
+}
+
+def calculator():
+ 	print (logo)
+	
+ 	n1 = float(input("What is the fisrt number?: "))
+	for symbol in operations:
+		print (symbol)
+	should_continue = True 
+
+	while should_continue:
+		operation_symbol = input("Pick an operation: ")
+		n2 = float(input("What is the next number?: "))
+		calculation_function = operations[operation_symbol]
+		answer = calculation_function(n1, n2)
+
+		print(f"{n1} {operation_symbol} {n2} = {answer}")
+	
+		if input(f"Type y to continue calculating with {answer} or type n to start a new 	calculation ") == "y":
+			n1 = answer
+		else:
+			should_continue = False
+			calculator()
+
+
+calculator()
